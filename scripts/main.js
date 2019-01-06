e91d736920d79e475a063aced01e4e078bbd6ea3
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-01-06 21:26:07 +0200

    making forms UI

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,17 @@
-"use strict"
-let $main = $("main");
-$main.accordion();
-$("section").tabs();
+$(()=>{
+	"use strict"
+	let $main = $("main");
+	$main.accordion();
+	$("#prizes").tabs();
+	$("#arrival").datepicker();
+	let $amount = $("#amount");
+	$("#price-range").slider({
+		range: true,
+		min: 0,
+		max: 400,
+		values: [175, 300],
+		slide: function(event, ui){
+			$amount.val(`\$${ui.values[0]} - \$${ui.values[1]}`);
+		}
+	});
+});
